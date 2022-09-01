3dcc2416b1672e841d3b5cd266d4eab142db3183
ramazan-aktas <111694852+ramazan-aktas@users.noreply.github.com>
2022-09-01 14:13:51 +0300

    Create RecursiveFibonacci.java
    Recursive Fibonacci to find nth fib number

---
+++ RecursiveFibonacci.java
@@ -0,0 +1,13 @@
+public class RecursiveFibonacci {
+	public static int nthFibNumber(int n)  {
+		return n == 0 ? 0 : 
+			n == 1 ? 1 :
+				nthFibNumber(n - 1) + nthFibNumber(n - 2);
+
+	}
+	public static void main(String[] args) {
+    nthFibNumber(0); // Returns 0
+		nthFibNumber(10); // Returns 55
+	}
+
+}
